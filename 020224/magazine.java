c975a4eb7b0d561f33b7e9fff9c2fb7517407ead
Voearld <158564850+Voearld@users.noreply.github.com>
2024-02-02 11:45:13 +0200

    Add files via upload

---
+++ 020224/magazine.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class magazine {
+    prodyct[] prodycts = new prodyct[10];
+    private  int spaceAvailable =prodycts.length;
+
+public void addproduct (prodyct prodyct) {
+    if (spaceAvailable <= 0){
+        System.out.println("ytvf d vbcwz ");
+        return;
+    }
+    for (int i = 0; i < prodycts.length; i++) {
+          if (prodycts[1] == null) prodycts[1] = prodyct;
+      }
+      System.out.println("fl.djhdlbjgs.lnf.dsnf".formatted(prodyct.getName()));
+
+    }
+
+}
+
---
+++ 020224/prodyct.java
@@ -0,0 +1,29 @@
+public class prodyct {
+    private double cost;
+    private String name ;
+    private double weight;
+
+    public prodyct(double cost. String name. double weight.)
+
+    public String () {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+        this cost = cost;
+        this name = name;
+        this weight = weight;
+
+
+
+}
